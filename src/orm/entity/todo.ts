d115672d08fa44489f5f049d0c3e46a51b924f6a
Wenszel <wiktor.smaga13@gmail.com>
2024-10-13 09:24:38 +0200

    Add sqlite database

---
+++ src/orm/entity/todo.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Todo {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column("text")
+    title: string;
+
+    @Column()
+    completed: boolean;
+
+    @Column({ nullable: true })
+    dueTo: string;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
+import "reflect-metadata";
+
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 import typeDefs from './graphql/typeDefs.js';
 import resolvers from './graphql/resolvers.js';
 
+import { AppDataSource } from './orm/datasource.js';
+
 const PORT: number = 8080;
 
+AppDataSource.initialize()
+  .then(() => {
+    console.log("[INFO] Database connected");
+  })
+  .catch((error) => console.log(error));
+
 const server = new ApolloServer({ typeDefs, resolvers });
-await startStandaloneServer(server, {
+const { url } = await startStandaloneServer(server, {
     listen: { port: PORT },
 });
+
+console.log(`[INFO] Server ready at ${url}`);

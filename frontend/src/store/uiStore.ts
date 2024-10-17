8d237efb403940c253fb59ce135a5b04fdd56b06
Wenszel <wiktor.smaga13@gmail.com>
2024-10-17 16:56:05 +0200

    Add ContextMenu on todo right click

--- frontend/src/store/uiStore.ts
+++ frontend/src/store/uiStore.ts
@@ -1,16 +1,33 @@
 import { StateCreator } from 'zustand';
+import Todo from '../interfaces/Todo';
 
 type State = {
+    xContextMenu: number;
+    yContextMenu: number;
+    showContextMenu: boolean;
     showNewList: boolean;
+    modifyingTodo: Todo | null;
 };
 
 type Action = {
+    setXContextMenu: (x: number) => void;
+    setYContextMenu: (y: number) => void;
+    setShowContextMenu: (show: boolean) => void;
     setShowNewList: (show: boolean) => void;
+    setModifyingTodo: (todo: Todo | null) => void;
 };
 
 export type UIStoreSlice = State & Action;
 
 export const createUIStoreSlice: StateCreator<UIStoreSlice, [], [], UIStoreSlice> = set => ({
     showNewList: false,
+    showContextMenu: false,
+    xContextMenu: 0,
+    yContextMenu: 0,
+    modifyingTodo: null,
+    setXContextMenu: (x: number) => set({ xContextMenu: x }),
+    setYContextMenu: (y: number) => set({ yContextMenu: y }),
+    setShowContextMenu: (show: boolean) => set({ showContextMenu: show }),
     setShowNewList: (show: boolean) => set({ showNewList: show }),
+    setModifyingTodo: (todo: Todo | null) => set({ modifyingTodo: todo }),
 });
